76c10aeb96510020f5f20fad4ed9afa99f0fb2e2
jareddodson <jared.dodson04@gmail.com>
2021-10-24 18:01:18 -0500

    fixed routes and put folders in correct folder

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const { db } = require('./Develop/db/db.json');
+const { db } = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
@@ -15,24 +15,25 @@ app.use (express.static('public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
+});*/
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // API Routes
 app.get('/api/notes', (req, res) => {
+    console.log('test text');
     return res.json(db);
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
+    let newNote = req.body;
     notes.push(newNote);
     res.json(req.body);
 })
