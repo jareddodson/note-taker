3b7539d59886183bbc34e5f2955b11cdb0f22426
jareddodson <jared.dodson04@gmail.com>
2021-10-24 20:58:50 -0500

    fixed post route and added notes in db.json

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const { db } = require('./db/db.json');
+const { notes } = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +12,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use (express.static('public'));
+
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+function validateNote(note) {
+    if (note.title === ""|| typeof note.title !== "string") {
+        return false;
+    }
+    if (note.text === "" || typeof note.text !== "string") {
+        return false;
+    }
+    return true
+}
 
 // HTML Routes
 app.get('/notes', (req, res) => {
@@ -28,8 +48,8 @@ app.get('/', (req, res) => {
 
 // API Routes
 app.get('/api/notes', (req, res) => {
-    console.log('test text');
-    return res.json(db);
+    let results = notes;
+    return res.json(results);
 });
 
 app.post('/api/notes', (req, res) => {
